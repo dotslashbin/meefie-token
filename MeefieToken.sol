5c21efc0d5ba2e2ddecee5477f976fc85e160444
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2023-09-12 22:57:41 -0400

    added basic getters

--- MeefieToken.sol
+++ MeefieToken.sol
@@ -1,12 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @custom:security-contact joshuarpf@gmail.com
 contract Meefie is ERC20, ERC20Burnable {
-    constructor() ERC20("Meefie", "MTF18") {
+
+    address _taxWallet;
+    
+    uint256 _multiplier;
+    uint256 _burn;
+    uint256 _tax;
+    
+
+    constructor(uint256 multipliier_, address taxWallet_, uint256 burn_, uint256 tax_) ERC20("Meefie", "MTF19") {
         _mint(msg.sender, 100000000000 * 10 ** decimals());
+
+        _taxWallet = taxWallet_;
+        _multiplier = multipliier_;
+        _burn = burn_;
+        _tax = tax_;
+    }
+
+    function burn() public returns (address) {
+        return _burn;
+    }
+
+    function multiplier()  returns (uint256) {
+        return _multiplier;
+    }
+
+    function setTaxWallet(address walletAddress_) onlyOwner public {
+        _taxWallet = walletAddress_;
+    }
+
+    function taxWallet() public returns (address) {
+        return _taxWallet;
     }
 }
