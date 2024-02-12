8c42b69c79b5984d54184c4c9c1e6dd8e02b1afa
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-02-11 23:29:00 -0500

    WIP: initial attempt of cleanup

--- MeefieToken.sol
+++ MeefieToken.sol
@@ -14,15 +14,14 @@ contract MEEFIE is Context, IERC20, Ownable {
     string private constant _symbol = "MFIE44";
     uint8 private constant _decimals = 18;
 
-    mapping(address => uint256) private _rOwned;
+    mapping(address account => uint256) private _balances;
+    mapping(address => uint256) private _taxWallet;
     mapping(address => uint256) private _tOwned;
     mapping(address => mapping(address => uint256)) private _allowances;
     mapping(address => bool) private _isExcludedFromFee;
 
     uint256 private constant MAX = ~uint256(0);
     uint256 private constant _tTotal = 100000000 * 10**_decimals;
-    uint256 private _rTotal = (MAX - (MAX % _tTotal));
-    uint256 private _tFeeTotal;
     uint256 private _taxFeeOnBuy = 25;
     uint256 private _taxFeeOnSell = 30;
 
@@ -54,8 +53,6 @@ contract MEEFIE is Context, IERC20, Ownable {
 
     constructor() {
 
-        _rOwned[_msgSender()] = _rTotal;
-
         IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
         uniswapV2Router = _uniswapV2Router;
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
@@ -86,7 +83,9 @@ contract MEEFIE is Context, IERC20, Ownable {
     }
 
     function balanceOf(address account) public view override returns (uint256) {
-        return tokenFromReflection(_rOwned[account]);
+        // TODO: impelment balance of without the reflection
+        //return tokenFromReflection(_rOwned[account]);
+        return _balances[account];
     }
 
     function transfer(address recipient, uint256 amount)
@@ -133,19 +132,6 @@ contract MEEFIE is Context, IERC20, Ownable {
         return true;
     }
 
-    function tokenFromReflection(uint256 rAmount)
-        private
-        view
-        returns (uint256)
-    {
-        require(
-            rAmount <= _rTotal,
-            "Amount must be less than total reflections"
-        );
-        uint256 currentRate = _getRate();
-        return rAmount.div(currentRate);
-    }
-
     function removeAllFee() private {
         if (_taxFee == 0) return;
 
@@ -168,11 +154,7 @@ contract MEEFIE is Context, IERC20, Ownable {
         emit Approval(owner, spender, amount);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 amount
-    ) private {
+    function _transfer( address from, address to, uint256 amount) private {
         require(from != address(0), "ERC20: transfer from the zero address");
         require(to != address(0), "ERC20: transfer to the zero address");
         require(amount > 0, "Transfer amount must be greater than zero");
@@ -258,81 +240,42 @@ contract MEEFIE is Context, IERC20, Ownable {
         sendETHToFee(contractETHBalance);
     }
 
-    function _tokenTransfer(
-        address sender,
-        address recipient,
-        uint256 amount,
-        bool takeFee
-    ) private {
+    function _tokenTransfer( address sender, address recipient, uint256 amount, bool takeFee) private {
         if (!takeFee) removeAllFee();
         _transferStandard(sender, recipient, amount);
         if (!takeFee) restoreAllFee();
     }
 
     function _transferStandard( address sender, address recipient, uint256 tAmount ) private {
-        ( uint256 rAmount, uint256 rTransferAmount, uint256 rFee, uint256 tTransferAmount, uint256 tFee, uint256 tTeam ) = _getValues(tAmount);
+        ( uint256 tTransferAmount, uint256 tTeam ) = _getValues(tAmount);
         
-        _rOwned[sender] = _rOwned[sender].sub(rAmount);
-        _rOwned[recipient] = _rOwned[recipient].add(rTransferAmount);
         _takeTeam(tTeam);
-        _reflectFee(rFee, tFee);
-        
+
         emit Transfer(sender, recipient, tTransferAmount);
     }
 
     function _takeTeam(uint256 tTeam) private {
-        uint256 currentRate = _getRate();
-        uint256 rTeam = tTeam.mul(currentRate);
-        _rOwned[address(this)] = _rOwned[address(this)].add(rTeam);
-    }
+        // uint256 currentRate = _getRate();
+        // uint256 rTeam = tTeam.mul(currentRate);
+        // _rOwned[address(this)] = _rOwned[address(this)].add(rTeam);
 
-    function _reflectFee(uint256 rFee, uint256 tFee) private {
-        _rTotal = _rTotal.sub(rFee);
-        _tFeeTotal = _tFeeTotal.add(tFee);
+        _taxWallet[address(this)] = _taxWallet[address(this)].add(tTeam);
     }
 
     receive() external payable {}
 
-    function _getValues(uint256 tAmount) private view returns ( uint256, uint256, uint256, uint256, uint256, uint256 )
+    function _getValues(uint256 tAmount) private view returns ( uint256, uint256 )
     {
-        (uint256 tTransferAmount, uint256 tFee, uint256 tTeam) = _getTValues(tAmount, _redisFee, _taxFee);
+        (uint256 tTransferAmount, uint256 tTeam) = _getTValues(tAmount, _taxFee);
         
-        uint256 currentRate = _getRate();
-        
-        (uint256 rAmount, uint256 rTransferAmount, uint256 rFee) = _getRValues(tAmount, tFee, tTeam, currentRate);
-        
-        return (rAmount, rTransferAmount, rFee, tTransferAmount, tFee, tTeam);
+        return (tTransferAmount, tTeam);
     }
 
-    function _getTValues( uint256 tAmount, uint256 redisFee, uint256 taxFee ) private pure returns ( uint256, uint256, uint256)
+    function _getTValues( uint256 tAmount, uint256 taxFee ) private pure returns ( uint256, uint256)
     {
-        uint256 tFee = tAmount.mul(redisFee).div(100);
         uint256 tTeam = tAmount.mul(taxFee).div(100);
-        uint256 tTransferAmount = tAmount.sub(tFee).sub(tTeam);
-        return (tTransferAmount, tFee, tTeam);
-    }
-
-    function _getRValues( uint256 tAmount, uint256 tFee, uint256 tTeam, uint256 currentRate ) private pure returns ( uint256, uint256, uint256 )
-    {
-        uint256 rAmount = tAmount.mul(currentRate);
-        uint256 rFee = tFee.mul(currentRate);
-        uint256 rTeam = tTeam.mul(currentRate);
-        uint256 rTransferAmount = rAmount.sub(rFee).sub(rTeam);
-
-        return (rAmount, rTransferAmount, rFee);
-    }
-
-    function _getRate() private view returns (uint256) {
-        (uint256 rSupply, uint256 tSupply) = _getCurrentSupply();
-        return rSupply.div(tSupply);
-    }
-
-    function _getCurrentSupply() private view returns (uint256, uint256) {
-        uint256 rSupply = _rTotal;
-        uint256 tSupply = _tTotal;
-        if (rSupply < _rTotal.div(_tTotal)) return (_rTotal, _tTotal);
-
-        return (rSupply, tSupply);
+        uint256 tTransferAmount = tAmount.sub(tTeam);
+        return (tTransferAmount, tTeam);
     }
 
     function changeFee(uint256 taxFeeOnBuy, uint256 taxFeeOnSell) public onlyOwner {
@@ -370,4 +313,7 @@ contract MEEFIE is Context, IERC20, Ownable {
         }
     }
 
+    function getTaxWalletBalance(address account) public view returns (uint256) {
+        return _taxWallet[account];
+    }
 }
