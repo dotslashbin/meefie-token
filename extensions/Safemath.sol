9a44f12de274b3b28a0da5cc1e3c909be0025a4a
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2024-02-19 10:54:48 -0500

    Cleaned up and broken files into pieces

--- extensions/Safemath.sol
+++ extensions/Safemath.sol
@@ -1,47 +1,36 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.17;
 
 library SafeMath {
+
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
-        uint256 c = a + b;
-        require(c >= a, "SafeMath: addition overflow");
-        return c;
+        return a + b;
     }
 
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-        return sub(a, b, "SafeMath: subtraction overflow");
-    }
-
-    function sub(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
-        require(b <= a, errorMessage);
-        uint256 c = a - b;
-        return c;
+        return a - b;
     }
 
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-        if (a == 0) {
-            return 0;
-        }
-        uint256 c = a * b;
-        require(c / a == b, "SafeMath: multiplication overflow");
-        return c;
+        return a * b;
     }
-
+    
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
-        return div(a, b, "SafeMath: division by zero");
+        return a / b;
     }
 
-    function div(
-        uint256 a,
-        uint256 b,
-        string memory errorMessage
-    ) internal pure returns (uint256) {
-        require(b > 0, errorMessage);
-        uint256 c = a / b;
-        return c;
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
     }
+    
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) { //Contract By Techaddict
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+    
 }
